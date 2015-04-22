7df95f34fbf00a1d30f941c8b6a265e459845c66
doudou0o <bluecc021@gmail.com>
2015-04-23 00:12:09 +0800

    add .py

---
+++ solutions/Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2);
+int main(int argc, const char *argv[])
+{
+    std::vector<int> n1;
+    std::vector<int> n2;
+    n1.push_back(2);
+    findMedianSortedArrays(n1,n2);
+    return 0;
+}
+
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+{
+    int length1 = nums1.size();
+    int length2 = nums2.size();
+
+    return 1;
+}
+
+void findpos(int s,int e)
+{
+}
