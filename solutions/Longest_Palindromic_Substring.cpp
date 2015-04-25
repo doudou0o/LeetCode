cff2f9ad24dd3175d435d416ccabf2ca9af4c087
doudou0o <bluecc021@gmail.com>
2015-04-26 01:10:35 +0800

    finish one

--- solutions/Longest_Palindromic_Substring.cpp
+++ solutions/Longest_Palindromic_Substring.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 
@@ -12,9 +13,35 @@ string longestPalindrome(string s)
         news.push_back(s.at(i));
     }
     news.push_back('#');
-    std::vector<int> p (news.size());
+    std::vector<int> p (news.size(),0);
 
+    int cur = 0, right = 0;
+    for (int i = 0; i < news.size()-1; i++) {
+        int mirror = 2*cur - i;
+        p[i] = right > i ? std::min( right-i , p[mirror] ):0;
 
+        while ((i+1+p[i]<news.size())&&(i-1-p[i]>=0)&&news.at(i+1+p[i]) == news.at(i-1-p[i]))
+            p[i]++;
+
+        if (i+p[i]>right) {
+            cur = i;
+            right = i +p[i];
+        }
+    }
+
+//    for (int i = 0; i < p.size(); i++) {
+//        std::cout << p[i] << " ";
+//    }
+    int maxlen=1,pos=0;
+    for (int i = 1; i < news.size()-1; i++) {
+        if (maxlen < p[i]) {
+            maxlen = p[i];
+            pos = i;
+        }
+    }
+    p.clear();
+//    std::cout << std::endl << pos <<" "<< maxlen << std::endl;
+    return s.substr((pos + 1 - maxlen)/2 , maxlen);
 }
 int main(int argc, const char *argv[])
 {
